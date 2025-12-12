1b7c26c72fdb637d4ed1f4d8041a8c30a6fd12b9
allysonDrogal <allyson.batista@drogal.com.br>
2025-12-12 14:06:12 -0300

    tela inicial de cadastro 50% finalizada

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,57 @@
 import ButtonPage from "../../components/button-page";
 import InputText from "../../components/input-text";
+import { Container, Content, Form, Form_1, ImageText } from "./styles";
+import imgLogin from "../../assets/imageFace.png"
 
 export default function Login() {
   return (
-    <div>
-      <h1>
-        Login
-      </h1>
-      <InputText />
-      <ButtonPage />
-    </div>
+    <Container>
+      <Content>
+        <ImageText>
+          <h1>No Facebook você se conecta e compartilha o que quiser com quem é importante em sua vida.</h1>
+
+          <img 
+          style={{width: '80%', marginLeft: '10%', transform: 'scaleY(0.8)' }} 
+          src={imgLogin} alt="" 
+            
+          />
+        </ImageText>
+
+        <Form>
+        <div style={{ marginLeft: '8px', marginBottom: '10px'}}>
+          <h2>Abra uma conta</h2>
+          <br />
+          <h4>É gratuito e sempre será.</h4>
+        </div>
+          <Form_1>
+            <InputText
+              type="text"
+              placeholder="Nome"
+
+            />
+            <InputText
+              type="text"
+              placeholder="Sobrenome"
+
+            />
+          </Form_1>
+           <InputText
+              type="text"
+              placeholder="E-mail ou número de celular"
+
+            />
+            <InputText
+              type="text"
+              placeholder="Insira novamente o e-mail ou número de celular"
+
+            />
+            <InputText
+              type="text"
+              placeholder="Nova senha"
+
+            />
+        </Form>
+      </Content>
+    </Container>
   )
 }
---
+++ src/pages/Login/styles.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+ //background: #797878ff;
+ width: 100%;
+ height: 100%;
+ border-radius: 15px;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+`;
+
+export const Content = styled.div`
+ //background: #f2f2f2;
+ width: 80%;
+ height: 70%;
+ border-radius: 15px;
+ display: flex;
+ justify-content: space-between;
+`;
+
+export const ImageText = styled.div`
+ width: 100%;
+ height: 100%;
+
+ `;
+
+export const Form = styled.div`
+ //background: purple;
+ width: 100%;
+ height: 100%;
+`;
+
+export const Form_1 = styled.div`
+ display: flex;
+
+`;
