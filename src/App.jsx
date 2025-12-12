0515c84643987ce4b2f4f464338e29267d313e0d
allysonDrogal <allyson.batista@drogal.com.br>
2025-12-12 12:00:24 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AppRoutes from "./routes"
+//import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from "./shared/styles/GlobalStyles"
+
+function App() {
+    return (
+    <>
+   <GlobalStyles />
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
