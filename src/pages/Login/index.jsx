71472a558b92c4193b92c0b3fdeddc05423d67ab
allysonDrogal <allyson.batista@drogal.com.br>
2025-12-13 14:00:37 -0300

    tela de login e cadastro finalizada!

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,8 +4,18 @@ import { Container, Content, Form, Form_1, ImageText } from "./styles";
 import imgLogin from "../../assets/imageFace.png"
 import { Button } from "../../components/button-page/styles";
 import HeaderBase from "../../shared/Layouts/header";
+import { useState } from "react";
+import * as React from 'react';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 
 export default function Login() {
+
+  const [value, setValue] = React.useState(null);
+
   return (
     <Container>
      <HeaderBase />
@@ -53,11 +63,29 @@ export default function Login() {
               placeholder="Nova senha"
 
             />
+            <div style={{ marginTop: '10px', marginLeft: '10px', display: 'flex'}}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DatePicker']}>
+                  <DatePicker value={value} onChange={(newValue) => setValue(newValue)} />
+                </DemoContainer>
+              </LocalizationProvider>
+              <p style={{ fontSize: '12px', color: '#65676b', marginTop: '10px', marginLeft: '10px', width: '25%'}} >
+                Por que preciso informar minha data de nascimento?
+              </p>
+            </div>
+            <div style={{ gap: '10px', display: 'flex', marginLeft: '10px', marginTop: '10px'}}>
+              <div>
+               <input style={{ marginTop: '3px'}} type="checkbox" /> Feminino
+              </div>
+              <div>
+               <input style={{ marginTop: '3px'}} type="checkbox" /> Masculino
+              </div>
+            </div>
+
 
             <p style={{ fontSize: '12px', color: '#65676b', marginTop: '10px', marginLeft: '10px', width: '80%'}}>
               Ao clicar em Abrir uma conta, você concorda com nossos Termos, Política de Dados e Política de Cookies. Você pode receber notificações por SMS e pode optar por não recebê-las quando quiser.
             </p>
-
             <ButtonPage 
               variant="success"
               type="submit"
